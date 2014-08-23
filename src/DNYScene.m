9b3fd89f1e43f98c9110046543f479d638e1248b
Alex Belliotti <alexbelliotti@gmail.com>
2014-08-22 22:42:00 -0700

    skeleton scene base class

---
+++ src/DNYScene.m
@@ -0,0 +1,13 @@
+//
+//  DNYScene.m
+//  Donny
+//
+//  Created by Alex Belliotti on 8/22/14.
+//  Copyright (c) 2014 CrowdCompass. All rights reserved.
+//
+
+#import "DNYScene.h"
+
+@implementation DNYScene
+
+@end
