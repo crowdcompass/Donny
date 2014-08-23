2c11c9b1a5044a306e33dd5ae6cd24eb1b7f2cce
Ben Cullen-Kerney <ben@beancuke.com>
2014-08-22 22:13:17 -0700

    Add base Interaction

---
+++ Donny/DNYInteraction.h
@@ -0,0 +1,16 @@
+//
+//  DNYInteraction.h
+//  Donny
+//
+//  Created by Ben Cullen-Kerney on 8/22/14.
+//  Copyright (c) 2014 CrowdCompass. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "DNYCreatureModel.h"
+
+@interface DNYInteraction : NSObject
+
+@property (nonatomic, strong) DNYCreatureModel* creature;
+
+@end
---
+++ Donny/DNYInteraction.m
@@ -0,0 +1,13 @@
+//
+//  DNYInteraction.m
+//  Donny
+//
+//  Created by Ben Cullen-Kerney on 8/22/14.
+//  Copyright (c) 2014 CrowdCompass. All rights reserved.
+//
+
+#import "DNYInteraction.h"
+
+@implementation DNYInteraction
+
+@end
