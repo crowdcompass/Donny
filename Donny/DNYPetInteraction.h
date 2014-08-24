6be1f4f749defc27389be9e17c86ba41c5b3d781
Ben Cullen-Kerney <ben@beancuke.com>
2014-08-23 21:56:00 -0700

    wip pet interaction

---
+++ Donny/DNYPetInteraction.h
@@ -0,0 +1,13 @@
+//
+//  DNYPetInteraction.h
+//  Donny
+//
+//  Created by Ben Cullen-Kerney on 8/23/14.
+//  Copyright (c) 2014 CrowdCompass. All rights reserved.
+//
+
+#import "DNYInteraction.h"
+
+@interface DNYPetInteraction : DNYInteraction
+
+@end
